d85f9c174608979662da3d3346eb07d822844cb6
sromeroa <salvador.romaguilar@gmail.com>
2024-09-11 14:19:27 -0600

    Add files via upload

---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi Proyecto PHP</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<header>
+    <h1>Bienvenido a mi Proyecto PHP</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+    </nav>
+</header>
